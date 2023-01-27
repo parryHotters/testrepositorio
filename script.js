e8e2f36dc98ae96d009edcc6a4a4efe23945480e
Maldonado <gheri7777@gamil.com>
2023-01-27 14:08:16 -0400

    clase02 completada

---
+++ script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('[data-form-btn]');
+
+
+console.log(btn);
+
+
+btn.addEventListener("click",function(evento) {
+    evento.preventDefault();
+    const input =document.querySelector('[data-form-input]');
+    console.log(input.value);
+}
+);
+
+//listener
+
+
