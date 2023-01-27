a6dba3102ae8809d3eb49dbf6b3ca78cb039e1b0
YoGhe~ <HOTTERSPARRY@GMAIL.COM>
2023-01-27 18:48:27 -0400

    lo que sea

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-( () => {const btn = document.querySelector('[data-form-btn]');
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
     evento.preventDefault();
@@ -14,37 +16,19 @@ const createTask = (evento) => {
     titleTask.classList.add('task');
     titleTask.innerText = value;
     taskContent.appendChild(titleTask);
-    const content = `<i class="fas fa-trash-alt trashIcon icon"></i>` ;
   //task.innerHTML = content;
   task.appendChild(taskContent);
-
+  task.appendChild(deleteIcon());
   list.appendChild(task);
-    console.log(content);
-}
-
-
-console.log(btn);
-
+    
+};
 // arrow functions o funciones anonimas
 
 btn.addEventListener('click', createTask);
 
-const checkComplete = () => {
-    const i =document.createElement('i');
-    i.classList.add('far','fa-check-square','icon');
-    i.addEventListener('click',completeTask);
-    return i;
-};
-
-const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle('fas');
-    element.classList.toggle('completeIcon');
-    element.classList.toggle('far');
-};
 
 
-})()
+
   
 //listener
 
